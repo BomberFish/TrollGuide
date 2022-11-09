46f2891766247a9ac51cb2e152f028f4521a08d7
BomberFish <87151697+BomberFish@users.noreply.github.com>
2022-11-09 13:38:53 -0500

    literally rework the entire detection system because of an incorrect assumption

--- script.js
+++ script.js
@@ -6,16 +6,11 @@ const shareData = {
 
 function isiOS() {
     const ua = navigator.userAgent;
-    return ua.indexOf("iPhone") > 0 || ua.indexOf("iPad") > 0 || ua.indexOf("iPod") > 0;
+    return ua.indexOf("iPhone") > 0 || ua.indexOf("iPad") > 0 || ua.indexOf("iPod touch") > 0;
 }
 
-function checkBetaCompat() {
-    const ua = navigator.userAgent;
-    if (ua.indexOf("19G5027e") > 0 || ua.indexOf("19G5037d") > 0 || ua.indexOf("19G5046d") > 0 || ua.indexOf("19G5056c") > 0 || ua.indexOf("19G5063a") > 0 || ua.indexOf("19E5209h") > 0 || ua.indexOf("19E5219e") > 0 || ua.indexOf("19E5225g") > 0 || ua.indexOf("19E5235a") > 0) {
-        return true;
-    } else {
-        return false;
-    }
+if(isiOS() == false) {
+    document.getElementById("notidevice").style.display="block"
 }
 
 function getVersion() {
@@ -46,28 +41,13 @@ function betaCompat() {
 function getCompat() {
     if (isiOS() == true && versionCompat() == true) {
         return true
-    } else if (isiOS() == true && betaCompat() == true && checkBetaCompat() == true) {
+    } else if (isiOS() == true && betaCompat() == true) {
         return true
     } else {
         return false
     }
 }
 
-function checkra1n() {
-    if (getVersion() < 1482 && getVersion() > 1400) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function checkRamdisk() {
-    if(checkBetaCompat() == true && getVersion() == 1560) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
 function check155Compat() {
     if(getVersion() < 1551 && betaCompat() == true) {
@@ -81,10 +61,7 @@ console.log("isiOS() returns", isiOS())
 console.log("getVersion() returns", getVersion())
 console.log("versionCompat() returns", versionCompat())
 console.log("betaCompat() returns", betaCompat())
-console.log("checkBetaCompat() returns", checkBetaCompat())
 console.log("getCompat() returns", getCompat())
-console.log("checkRamdisk() returns", checkRamdisk())
-console.log("checkra1n() returns", checkra1n())
 console.log("check155Compat() returns", check155Compat())
 
 if (getCompat() == false) {
@@ -92,10 +69,20 @@ if (getCompat() == false) {
     document.getElementById("get").disabled = "true"
     document.getElementById("get").style.width = "9em"
     document.getElementById("get").style.background = "var(--gray1)"
-} else if(checkRamdisk() == true) {
+} 
+
+if(getVersion() == 1560) {
     document.getElementById("ramdisk").style.display = "block"
-} else if(checkra1n() == true) {
+} 
+
+if(getVersion() < 1482 && getVersion() > 1399) {
     document.getElementById("checkra1n").style.display = "block"
+}
+
+if (getVersion() == 1560) {
+    document.getElementById("156").style.display = "block"
+} else if (getVersion() == 1550) {
+    document.getElementById("155").style.display = "block"
 }
 
 
